28d0703a47bec93aa0088d461247719b51dd028c
Clarissa Estremos <cestremos@gmail.com>
2015-09-18 19:54:49 +0800

    Save on database

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -4,7 +4,21 @@ angular.module('SteroidsApplication', [
 .controller('IndexController', ['$scope', '$http', 'supersonic', function($scope, $http, supersonic) {
     
     $scope.data = [];
-    for(var i = 1; i<=5; i++){
+    var db = window.openDatabase("DB name",1, "Display name",200000);
+//    var flag = 0;
+//    db.transaction(function(transaction){
+//        transaction.executeSql("drop table if exists songArtist",[],function(transaction,result){
+//            console.log("dropped!");
+//        })
+//    });
+    
+    db.transaction(function(transaction){
+        transaction.executeSql("create table if not exists songArtist(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, picture TEXT)",[],function(transaction,result){
+            console.log("table created!");
+        })
+    });
+    
+    for(var i = 1; i<=10; i++){
         $.ajax({
             dataType: "jsonp",
             url :"http://api.deezer.com/2.0/artist/"+i+"?output=jsonp",
@@ -13,11 +27,40 @@ angular.module('SteroidsApplication', [
             success : function(data) {
                
                  $scope.$apply(function () {
-                    $scope.data.push(data);
+                    
+                   db.transaction(function(transaction,result){
+                        transaction.executeSql("INSERT INTO songArtist (name, picture) values ('"+data.name+"','"+data.picture+"')",[],function(transaction, result){
+                        }, function(transaction, error){
+                           // alert("Error inserting data.", error);
+                        });
+                    });
+                   
                 });
+            },
+            error: function(e){
+                alert("Cannot connect to the server.");
             }
           });
         
     }
+    
+    
+    var selectQuery = "select * from songArtist"; 
+        db.transaction(function(transaction) {
+            transaction.executeSql(""+selectQuery, [], function(transaction, result) {
+                if (result != null) {
+                    for (var i = 0; i < result.rows.length; i++) {
+                      var row = result.rows.item(i);
+                      $scope.data.push(row);
+                    }
+                    
+                  }else{
+                      alert("No data retrieved. Check your connection.");
+                  }
+            }, function(e) {
+              alert("ERROR Select Query: " + e.message);
+            });
+        });
+    
 
 }]);
