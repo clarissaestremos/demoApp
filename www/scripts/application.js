e21d315e31d524a4578907a788317d8fa87a8fbe
Clarissa Estremos <cestremos@gmail.com>
2015-09-22 18:53:53 +0800

    Use service to fetch from url gave by carmx

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -1,63 +1,85 @@
-angular.module('SteroidsApplication', [
-  'supersonic'
-])
-.controller('IndexController', ['$scope', '$http', 'supersonic', function($scope, $http, supersonic) {
-    
-    $scope.data = [];
-    
-    var responsePromise = $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London");
+var myApp = angular.module('SteroidsApplication',['supersonic']);
+
+
+myApp.service('DataService', function($http) {
+  var promise;
+  var myService = {
+    getData: function() {
+      if ( !promise ) {
+        // $http returns a promise, which has a then function, which also returns a promise
+        promise = $http.get('https://glacial-harbor-7075.herokuapp.com/musicArtist/list').then(function (response) {
+          // The then function here is an opportunity to modify the response
+          console.log(response);
+          // The return value gets picked up by the then in the controller.
+          return response.data;
+        });
+      }
+      // Return the promise to the controller
+      return promise;
+    }
+  };
+  return myService;
+});
+
+myApp.controller('IndexController', ['supersonic', 'DataService', '$scope',function(supersonic, DataService,$scope) {
+
     var db = window.openDatabase("DB name",1, "Display name",200000);
-    responsePromise.success(function(data, status, headers, config) {
-       
+    $scope.listArtist = [];
+//    $scope.showData = function(){
+        DataService.getData().then(function(data) {
+           
+            db.transaction(function(transaction){
+                transaction.executeSql("create table if not exists songArtist(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, picture TEXT)");
+
+
+                transaction.executeSql("select * from songArtist", [], function(transaction, result) {
+
+                    if (result.rows.length>=1) {
+                        $scope.listArtist = data;
+                    }else if (result.rows.length<1){ //first time to use the app
+                        for(d of data){
+                            transaction.executeSql("INSERT INTO songArtist (name, picture) values ('"+d.name+"', '"+d.picture+"')");
+                        }
+                          
+                       select();
+
+                      }
+                });
+            });
+
+        }, function(reason) {
+            select();
+        });
+    
+    function select(){
+        db.transaction(function(transaction){
+            transaction.executeSql("select * from songArtist", [], function(transaction, result) {
+                    for (var i = 0; i < result.rows.length; i++) {
+                      var row = result.rows.item(i);
+                       $scope.$apply(function () {  
+                           $scope.listArtist.push(row);
+                        });
+                    }
+            });
+        });  
+    }
+        
+//    }
+//    
+//    var responsePromise = $http.get("https://glacial-harbor-7075.herokuapp.com/musicArtist/list");
+    //alert(responsePromise.);
+    
+
+//    responsePromise.then(function(response) {
+//       alert(response);
 //          db.transaction(function(transaction){
 //        transaction.executeSql("drop table if exists songArtist",[],function(transaction,result){
 //            alert("dropped!");
 //        })
 //    });
         
-          db.transaction(function(transaction){
-            transaction.executeSql("create table if not exists songArtist(id INTEGER PRIMARY KEY AUTOINCREMENT, name INT)");
-
-
-            transaction.executeSql("select * from songArtist", [], function(transaction, result) {
-
-                if (result.rows.length>=1) {
-                    for (var i = 0; i < result.rows.length; i++) {
-                      var row = result.rows.item(i);
-                       $scope.$apply(function () {  
-                           $scope.data.push(row);
-                        });
-                    }
-
-                  }else if (result.rows.length<1){ 
-                    for(d of data.list){
-                        transaction.executeSql("INSERT INTO songArtist (name) values ("+d.dt+")");
-                    }
-
-                   getData();
-
-                  }
-            });
-        });
-                 
-    });
-    responsePromise.error(function(data, status, headers, config) {
-        getData();
-    });
-    
-    function getData(){
-        db.transaction(function(transaction){
-            transaction.executeSql("select * from songArtist", [], function(transaction, result) {
-                    for (var i = 0; i < result.rows.length; i++) {
-                      var row = result.rows.item(i);
-                       $scope.$apply(function () {  
-                           $scope.data.push(row);
-                        });
-                    }
-            });
-        });  
-    }
-
+          
 }]);
+
 
 
