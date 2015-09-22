66f001fe0137164ff1c2616ac148eb3580d94552
Clarissa Estremos <cestremos@gmail.com>
2015-09-22 14:46:58 +0800

    Get data from single url with internet connection or local database with no internet connection

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -4,63 +4,60 @@ angular.module('SteroidsApplication', [
 .controller('IndexController', ['$scope', '$http', 'supersonic', function($scope, $http, supersonic) {
     
     $scope.data = [];
+    
+    var responsePromise = $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London");
     var db = window.openDatabase("DB name",1, "Display name",200000);
-//    var flag = 0;
-//    db.transaction(function(transaction){
+    responsePromise.success(function(data, status, headers, config) {
+       
+//          db.transaction(function(transaction){
 //        transaction.executeSql("drop table if exists songArtist",[],function(transaction,result){
-//            console.log("dropped!");
+//            alert("dropped!");
 //        })
 //    });
-    
-    db.transaction(function(transaction){
-        transaction.executeSql("create table if not exists songArtist(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, picture TEXT)",[],function(transaction,result){
-            console.log("table created!");
-        })
-    });
-    
-    for(var i = 1; i<=10; i++){
-        $.ajax({
-            dataType: "jsonp",
-            url :"http://api.deezer.com/2.0/artist/"+i+"?output=jsonp",
-            data : {},
-            jsonp : 'callback',
-            success : function(data) {
-               
-                 $scope.$apply(function () {
-                    
-                   db.transaction(function(transaction,result){
-                        transaction.executeSql("INSERT INTO songArtist (name, picture) values ('"+data.name+"','"+data.picture+"')",[],function(transaction, result){
-                        }, function(transaction, error){
-                         
-                        });
-                    });
-                   
-                });
-            },
-            error: function(e){
-                alert("Cannot connect to the server.");
-            }
-          });
         
-    }
-    
-    
-    var selectQuery = "select * from songArtist"; 
-        db.transaction(function(transaction) {
-            transaction.executeSql(""+selectQuery, [], function(transaction, result) {
-                if (result != null) {
+          db.transaction(function(transaction){
+            transaction.executeSql("create table if not exists songArtist(id INTEGER PRIMARY KEY AUTOINCREMENT, name INT)");
+
+
+            transaction.executeSql("select * from songArtist", [], function(transaction, result) {
+
+                if (result.rows.length>=1) {
                     for (var i = 0; i < result.rows.length; i++) {
                       var row = result.rows.item(i);
-                      $scope.data.push(row);
+                       $scope.$apply(function () {  
+                           $scope.data.push(row);
+                        });
                     }
-                    
-                  }else{
-                      alert("No data retrieved. Check your connection.");
+
+                  }else if (result.rows.length<1){ 
+                    for(d of data.list){
+                        transaction.executeSql("INSERT INTO songArtist (name) values ("+d.dt+")");
+                    }
+
+                   getData();
+
                   }
-            }, function(e) {
-              alert("ERROR Select Query: " + e.message);
             });
         });
+                 
+    });
+    responsePromise.error(function(data, status, headers, config) {
+        getData();
+    });
     
+    function getData(){
+        db.transaction(function(transaction){
+            transaction.executeSql("select * from songArtist", [], function(transaction, result) {
+                    for (var i = 0; i < result.rows.length; i++) {
+                      var row = result.rows.item(i);
+                       $scope.$apply(function () {  
+                           $scope.data.push(row);
+                        });
+                    }
+            });
+        });  
+    }
 
 }]);
+
+
