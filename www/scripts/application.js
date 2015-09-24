25a17a9d0d2dab8b702400428e9ebde4b1cb8081
Danica <danicamozo@gmail.com>
2015-09-24 11:07:07 +0800

    added pages, changed javascript code

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -44,103 +44,103 @@ myApp.config(function($routeProvider) {
         controller: 'IndexController'
     })
     
+    .when('/favorite', {
+        templateUrl: 'pages/favorite.html',
+        controller: 'IndexController'
+    })
+    
 });
 
 myApp.service('DataService', function($http) {
-  var promise;
-  var myService = {
-    getData: function() {
-      if ( !promise ) {
-        // $http returns a promise, which has a then function, which also returns a promise
-        promise = $http.get('https://glacial-harbor-7075.herokuapp.com/musicArtist/list').then(function (response) {
-          // The then function here is an opportunity to modify the response
-          console.log(response);
-          // The return value gets picked up by the then in the controller.
-          return response.data;
+var self = this;
+  
+    self.getData = function(){
+        var deferred = $q.defer(),
+            url = 'https://glacial-harbor-7075.herokuapp.com/musicArtist/list';
+        $http.get(url).success(function(result){
+            deferred.resolve(result);
+        }).error(function(err){
+            deferred.reject(err);     
         });
-      }
-      // Return the promise to the controller
-      return promise;
+        return deferred.promise;
     }
-  };
-  return myService;
+  return self;
 });
 
 myApp.controller('IndexController', ['supersonic', 'DataService', '$scope',function(supersonic, DataService,$scope) {
     
     var db = window.openDatabase("DB name",1, "Display name",200000);
-    $scope.listArtist = [];
+    
+    var list = [];
+    $scope.listArtist1 = [];
+    $scope.listArtist2 = [];
+    $scope.numData1 = 0;
+    $scope.numData2 = 0;
     $scope.search_input="";
 
-//    $scope.showData = function(){
-        DataService.getData().then(function(data) {
-           
+    DataService.getData().then(function(data) {
             db.transaction(function(transaction){
                 
                 transaction.executeSql("create table if not exists songArtist(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, picture TEXT)");
-
                 transaction.executeSql("select * from songArtist", [], function(transaction, result) {
 
                     
-                    if (result.rows.length>=1) {           
-                        $scope.$apply(function () {  
-                            $scope.listArtist = data;
-                        });
-                        
-                        var nativeJavascriptListArtist = data;
-                        var ul = document.getElementById("nativeAddArtist");
-                        
-                      for (var p in nativeJavascriptListArtist) {
-                        if( nativeJavascriptListArtist.hasOwnProperty(p) ) {
-                          ul.innerHTML += "<li>"+"<img src='"+nativeJavascriptListArtist[p].picture+"'/><br>"+nativeJavascriptListArtist[p].name+"</li>";
-                        } 
-                      } 
-                        
-                    }else if (result.rows.length<1){ //first time to use the app
+                   if (result.rows.length<1){ //first time to use the app
                         for(d of data){
                             transaction.executeSql("INSERT INTO songArtist (name, picture) values ('"+d.name+"', '"+d.picture+"')");
                         }
-                       
-                        $scope.$apply(function () {  
-                            $scope.listArtist = data;
-                        });  
-
                       }
+                        
                 });
             });
-
+            
         }, function(reason) {
-            select();
-        });
-    
-    function select(){
-        db.transaction(function(transaction){
-            transaction.executeSql("select * from songArtist", [], function(transaction, result) {
-                    for (var i = 0; i < result.rows.length; i++) {
-                      var row = result.rows.item(i);
-                       $scope.$apply(function () {  
-                           $scope.listArtist.push(row);
-                        });
-                    }
-            });
+            alert("No internet connection.");
         });  
-    }      
-//}
-//    
-//    var responsePromise = $http.get("https://glacial-harbor-7075.herokuapp.com/musicArtist/list");
-    //alert(responsePromise.);
     
-
-//    responsePromise.then(function(response) {
-//       alert(response);
-//          db.transaction(function(transaction){
-//        transaction.executeSql("drop table if exists songArtist",[],function(transaction,result){
-//            alert("dropped!");
-//        })
+    $scope.showData = function(){
+        var dt = new Date();
+        var timer = dt.getMilliseconds();
+        $scope.timer = 0;
+        $scope.listOfArtist1 = dataQueries(timer);
+        $scope.timer = timer;
+        $scope.numData1 = $scope.listOfArtist1.length;
+    }
+    
+//    document.getElementById("btnClick").addEventListener("click", function(){
+//        var dt = new Date();
+//        var timer2 = dt.getMilliseconds();
+//        $scope.timer2 = 0;
+//        $scope.listOfArtist2 = dataQueries(timer2);
+//        $scope.timer2 = timer2;
+//        $scope.numData2 = $scope.listOfArtist2.length;
 //    });
     
     $scope.search = function(){
         alert($scope.search_input);
+    }
+    
+    function dataQueries(timer){
+        DataService.getData().then(function(data) {
+            list = data;
+        }, function(reason) {
+            db.transaction(function(transaction){
+                transaction.executeSql("select * from songArtist", [], function(transaction, result) {
+                      
+                        for (var i = 0; i < result.rows.length; i++) {
+                          var row = result.rows.item(i);
+                           $scope.$apply(function () {  
+                               list.push(row);
+                            });
+                        }
+                });
+            });  
+        });
+
+        
+        var dt2 = new Date();
+        timer = (dt2.getMilliseconds() - timer)/1000;
+        return list;
     }
           
 }]);
